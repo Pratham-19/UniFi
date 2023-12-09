931093aee03b14c0b74674df6a9bb93f7c732506
Pratham <prathamsinghal2003@gmail.com>
2023-12-09 15:56:44 +0530

    feat: add airstack queries

---
+++ client/app/try/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import { useQuery, init } from "@airstack/airstack-react";
+import { GetAddressFromEns } from "../_lib/airstack_queries";
+
+init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY ?? "");
+
+const Page = () => {
+    const { data, loading, error } = useQuery(GetAddressFromEns, {
+            name: "megabyte0x.eth",
+        },
+    );
+    console.log(data, loading, error);
+
+    return <div>Page</div>;
+};
+
+export default Page;

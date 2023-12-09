c176ec13d9191e0ae192a72327b8c1afcd652781
L-Pircy <taveesha.agarwal@gmail.com>
2023-12-09 13:30:56 +0530

    landing designing fin

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -56,12 +56,12 @@ export default function Home() {
           width={380}
           className="rounded-[16px]"
         />
-        <div className="flex flex-col gap-8">
-          <h1 className="text-[54px] text-[#ffffff] font-semibold">
-            Transfer between any chain
+        <div className="flex flex-col gap-6 px-16 self-center items-center justify-centers">
+          <h1 className="text-[45px] text-[#ffffff] font-semibold text-center">
+            Transfer between any chain.
           </h1>
-          <div className="flex flex-row justify-center items-center gap-4">
-            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-2 flex justify-center items-center gap-[10px]">
+          <div className="flex flex-row justify-center items-center gap-4 font-semibold">
+            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-1 flex justify-center items-center gap-[10px]">
               <Image
                 src={'/mainLanding/icon-people.png'}
                 alt="landing"
@@ -69,9 +69,9 @@ export default function Home() {
                 width={61}
                 className="max-h-[24px] min-w-[61]"
               />
-              <h4 className="text-[32px] text-[#ffffff] ">To anyone</h4>
+              <h4 className="text-[21px] text-[#ffffff] ">To anyone</h4>
             </div>
-            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-2 flex gap-[10px]">
+            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-1 flex gap-[10px]">
               <Image
                 src={'/mainLanding/icon-globe.svg'}
                 alt="landing"
@@ -79,9 +79,9 @@ export default function Home() {
                 width={30}
                 className=""
               />
-              <h4 className="text-[32px] text-[#ffffff] ">Anywhere</h4>
+              <h4 className="text-[21px] text-[#ffffff] ">Anywhere</h4>
             </div>
-            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-2 flex gap-[10px]">
+            <div className="rounded-full w-fit border-solid border-[1px] border-[#ffffff] px-4 py-1 flex gap-[10px]">
               <Image
                 src={'/mainLanding/icon-time.svg'}
                 alt="landing"
@@ -89,50 +89,60 @@ export default function Home() {
                 width={33}
                 className=""
               />
-              <h4 className="text-[32px] text-[#ffffff] ">At Anytime</h4>
+              <h4 className="text-[21px] text-[#ffffff] ">At Anytime</h4>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-row gap-[24px] py-[48px]">
-        <div className="flex pr-8 rounded-[32px] bg-[radial-gradient(1508.38%_141.42%_at_0%_100%,_#D2FFA6_0%,_rgba(210,_255,_166,_0.20)_100%)]">
+      <div className="flex flex-row gap-[24px] py-[48px] font-semibold px-6 z-10">
+        <div className="flex pr-8 gap-6 rounded-[32px] bg-[radial-gradient(1508.38%_141.42%_at_0%_100%,_#D2FFA6_0%,_rgba(210,_255,_166,_0.20)_100%)]">
           <Image
             src={'/mainLanding/wallet.png'}
             alt="wallet"
             width={280}
             height={140}
           />
-          <div className="py-12 ">
+          <div className="py-12 self-center">
             <h5 className="text-[24px]">Experience the power of</h5>
-            <h3 className="text-[48px]">Smart wallets</h3>
+            <h3 className="text-[39px] leading-10">Smart wallets</h3>
           </div>
         </div>
-        <div className="flex pr-8 rounded-[32px] bg-[radial-gradient(1508.38%_141.42%_at_0%_100%,_#D8A6FF_0%,_rgba(216,_166,_255,_0.20)_100%)]">
+        <div className="flex pr-8 gap-4 rounded-[32px] bg-[radial-gradient(1508.38%_141.42%_at_0%_100%,_#D8A6FF_0%,_rgba(216,_166,_255,_0.20)_100%)]">
           <Image
             src={'/mainLanding/landing-ethusd.png'}
             alt="wallet"
             width={280}
             height={140}
           />
-          <div className="py-12 ">
+          <div className="py-12 self-center">
             <h5 className="text-[24px]">Start transferring</h5>
-            <h3 className="text-[48px]">Between chains</h3>
+            <h3 className="text-[39px] leading-10">Between chains</h3>
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-[10px]">
+      <div className="flex flex-col gap-[10px] z-10">
         <div className="flex flex-col gap-2 justify-center items-center text-center">
-          <h3 className="text-[48px]">Time to chill and stop worrying</h3>
-          <h1 className="text-[80px]">SOCIAL RECOVERY</h1>
-          <h3 className="text-[48px]">for your wallet and funds</h3>
+          <h3 className="text-[48px] font-normal">
+            Time to chill and stop worrying
+          </h3>
+          <h1 className="text-[80px] font-bold">SOCIAL RECOVERY</h1>
+          <h3 className="text-[48px] font-normal">for your wallet and funds</h3>
         </div>
         <Image
           src={'/mainLanding/shaking-hands.png'}
           alt="hanshake"
           height={500}
           width={666}
+          className="self-center"
         />
       </div>
+      <Image
+        src={'/mainLanding/landing-netbtm.png'}
+        alt="bg"
+        height={900}
+        width={1200}
+        className="absolute pt-[1200px] opacity-50 self-center z-0"
+      />
     </div>
   );
 }

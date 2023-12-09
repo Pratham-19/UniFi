c3cc845e27663b91ac512adfd6b69993490c13a8
Pratham <prathamsinghal2003@gmail.com>
2023-12-10 00:31:01 +0530

    fix: add pseudo login page

--- client/app/_components/Buttons/ConnectWallet.ts
+++ client/app/_components/Buttons/ConnectWallet.ts
@@ -1,17 +1,18 @@
-import { BrowserProvider } from 'ethers';
+"use client";
+import { BrowserProvider } from "ethers";
 
 declare var window: any;
 
 export const connectWallet = async () => {
-  if (window.ethereum) {
-    try {
-      const provider = new BrowserProvider(window.ethereum);
-      const signer = await provider.getSigner();
-      return signer?.address;
-    } catch (error) {
-      console.error(error);
+    if (window.ethereum) {
+        try {
+            const provider = new BrowserProvider(window.ethereum);
+            const signer = await provider.getSigner();
+            return signer?.address;
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.error("No web3 provider found");
     }
-  } else {
-    console.error('No web3 provider found');
-  }
 };

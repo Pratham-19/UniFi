a4ae2585323e5eed0f212546643e4aad3ba2fecc
L-Pircy <taveesha.agarwal@gmail.com>
2023-12-09 23:10:37 +0530

    nft and poap

--- client/app/mynfts/page.tsx
+++ client/app/mynfts/page.tsx
@@ -5,18 +5,18 @@ import Image from 'next/image';
 const MyNftPage = () => {
   return (
     <div className="w-[100vw] bg-[linear-gradient(299deg,_#FFFCEA_0%,_#FFF8D4_0.01%,_#F8FCFF_100%)]">
-      <div className="grid pt-[32px] gap-4">
+      <div className="grid pt-[21px] gap-4 pb-[51px]">
         <div className="self-start place-self-center grid grid-flow-col gap-4">
-          <Nft />
-          <Nft />
-          <Nft />
-          <Nft />
+          <Nft chain={1} />
+          <Poap />
+          <Poap />
+          <Nft chain={1} />
         </div>{' '}
         <div className="self-start place-self-center grid grid-flow-col gap-4">
-          <Nft />
+          <Nft chain={1} />
           <Poap />
-          <Nft />
-          <Nft />
+          <Nft chain={1} />
+          <Nft chain={1} />
         </div>
       </div>
       <Footer />

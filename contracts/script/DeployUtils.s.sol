4faac3b0df279b76fdac94e8958de4b5d72dd95e
megabyte0x <yashygkf@gmail.com>
2023-12-09 06:13:21 +0530

    add: deployment script for usdc and utils

---
+++ contracts/script/DeployUtils.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {Utils} from "../src/Utils.sol";
+
+contract DeployUtils is Script {
+    bytes32 public constant SALT = 0x556e794669000000000000000000000000000000000000000000000000000000;
+
+    function run() public {
+        vm.startBroadcast();
+        Utils utils = new Utils{salt:SALT}();
+        vm.stopBroadcast();
+        console.log("Utils Address: %s", address(utils));
+    }
+}

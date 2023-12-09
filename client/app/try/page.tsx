66c5a66f6cb23ffaa49089bc66ae6f38ecc930d2
Pratham <prathamsinghal2003@gmail.com>
2023-12-09 15:58:19 +0530

    feat: add airstack queries

--- client/app/try/page.tsx
+++ client/app/try/page.tsx
@@ -1,18 +1,17 @@
-"use client";
-import React from "react";
-import { useQuery, init } from "@airstack/airstack-react";
-import { GetAddressFromEns } from "../_lib/airstack_queries";
+'use client';
+import React from 'react';
+import { useQuery, init } from '@airstack/airstack-react';
+import { GetAddressFromEns } from '../_lib/airstack_queries';
 
-init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY ?? "");
+init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY ?? '');
 
 const Page = () => {
-    const { data, loading, error } = useQuery(GetAddressFromEns, {
-            name: "megabyte0x.eth",
-        },
-    );
-    console.log(data, loading, error);
+  const { data, loading, error } = useQuery(GetAddressFromEns, {
+    name: 'megabyte0x.eth'
+  });
+  console.log(data, loading, error);
 
-    return <div>Page</div>;
+  return <div>Page</div>;
 };
 
 export default Page;
